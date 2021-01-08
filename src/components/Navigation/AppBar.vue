6a73cf8bd50287a7f74cb3a284edc7dc8762be13
AlmogEdri <31370261+AlmogEdri@users.noreply.github.com>
2021-01-08 12:09:27 +0200

    insatall vuetify and axios created basic navigation

---
+++ src/components/Navigation/AppBar.vue
@@ -0,0 +1,18 @@
+<template>
+	<v-app-bar app color="primary" dark>
+		<v-toolbar-title >Weather</v-toolbar-title>
+		<v-spacer />
+		<nav-buttons class="d-flex align-right" />
+	</v-app-bar>
+</template>
+
+<script>
+	import NavButtons from './Buttons';
+	export default {
+		components: {
+			NavButtons
+		}
+	};
+</script>
+
+<style lang="scss"></style>
---
+++ src/components/Navigation/Buttons.vue
@@ -0,0 +1,19 @@
+<template>
+	<div>
+		<v-btn value="nearby">
+			<span>Nearby</span>
+			<v-icon>mdi-home</v-icon>
+		</v-btn>
+		
+		<v-btn value="favorites">
+			<span>Favorites</span>
+			<v-icon>mdi-heart</v-icon>
+		</v-btn>
+	</div>
+</template>
+
+<script>
+	export default {};
+</script>
+
+<style lang="scss"></style>
